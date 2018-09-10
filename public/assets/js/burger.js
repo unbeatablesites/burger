7bb815474fd77ec5202931103d015fb95bf580fe
Fuzoka <unbeatablesites09@gmail.com>
2018-09-10 00:15:30 -0400

    Updating app for Heroku

---
+++ public/assets/js/burger.js
@@ -0,0 +1,41 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+    var newDevouredState = {
+        devoured: 1
+    };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevouredState);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+        var newBurger = {
+        burger_name: $("#burger").val().trim(),
+        devoured: 0
+    };
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
