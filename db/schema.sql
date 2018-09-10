a8757f31eed106dcea1bb62c5f75294a375150d6
Fuzoka <unbeatablesites09@gmail.com>
2018-09-10 00:16:18 -0400

    Updating app for Heroku

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table tasks.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured boolean NOT NULL DEFAULT 0,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger Avacado');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheap Burger',1);
