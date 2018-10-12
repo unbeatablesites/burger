34e878e5c0dc83b7d004fe9cdaadd6947b3dc9d1
Fuzoka <unbeatablesites09@gmail.com>
2018-10-11 20:56:54 -0400

    Heroku Deploy

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_Controller.js");
 
-app.use(routes);
+app.use('/', routes);
 
 
 app.listen(PORT, function(){
