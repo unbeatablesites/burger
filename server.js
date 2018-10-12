fae5ba306df4edd6ddbffc8b4875ab2784e5c8f8
Fuzoka <unbeatablesites09@gmail.com>
2018-10-11 22:57:24 -0400

    Heroku Deploy

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+//var methodOverride = require('method-override');
 
-var PORT = process.env.PORT || 5000;
+// Open Server
+var PORT = process.env.PORT || 3000;
 
 var app = express();
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static('public'));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
-app.use('/', routes);
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
 
 
 
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("Express is working on port " + port);
+app.listen(PORT, function() 
+{
+  console.log("App listening on PORT " + PORT);
 });
