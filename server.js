e478c9e09dafb69d19b18ca038b6454d100a8cff
Fuzoka <unbeatablesites09@gmail.com>
2018-10-11 22:31:15 -0400

    Heroku Deploy

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ app.use('/', routes);
 
 
 
-app.listen(PORT, function() 
-{
-  console.log("App listening on PORT " + PORT);
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
